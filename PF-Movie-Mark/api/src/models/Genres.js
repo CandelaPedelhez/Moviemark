7589278b328acf59a091380892417312b9b45c64
Nacarith <nacariths@gmail.com>
2022-03-03 17:12:27 -0400

    Cambios en models genres, groceries

--- PF-Movie-Mark/api/src/models/Genres.js
+++ PF-Movie-Mark/api/src/models/Genres.js
@@ -2,11 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("genre", {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true, 
-    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
--- PF-Movie-Mark/api/src/models/Groceries.js
+++ PF-Movie-Mark/api/src/models/Groceries.js
@@ -2,11 +2,6 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("groceries", {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true, 
-    },
     name: {
       type: DataTypes.STRING,
     },
