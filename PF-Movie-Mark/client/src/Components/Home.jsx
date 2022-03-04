56eea25311a4da47b75e4017a380e0f1755faafd
Juan <jnmvrgr@gmail.com>
2022-03-04 05:57:32 -0300

    added top rated and upcoming section

--- PF-Movie-Mark/client/src/Components/Home.jsx
+++ PF-Movie-Mark/client/src/Components/Home.jsx
@@ -3,6 +3,10 @@ import { useSelector, useDispatch } from "react-redux";
 import NavBar from "../Components/Navbar";
 import { filterMovieByGenre } from "../Actions";
 import Slider from "./Slider/Slider.jsx"
+import { topRated } from "./constants/top_rated.js"
+import { upcoming } from "./constants/upcoming.js"
+import "./Home.css"
+
 export default function Home() {
   const dispatch = useDispatch();
   const movies = useSelector((state) => state.movies);
@@ -16,7 +20,7 @@ export default function Home() {
   return (
     <div>
       <NavBar />
-      <Slider/>
+      <Slider />
       <h2>Hola soy el Home</h2>
       <h3>Si lo sabemos</h3>
       <h4>Ah ok.</h4>
@@ -34,6 +38,31 @@ export default function Home() {
           );
         })}
       </select>
+      <div classname="row">
+        <h2>Top Rated</h2>
+        <div className="row__posters">
+          {topRated?.map((e) => (
+            <div>
+              {/* <h3 key={e.id}>{e.name}</h3> */}
+              <img key={e.id} src={e.img} className="row__poster" />
+            </div>
+          )
+          )}
+        </div>
+      </div>
+      <br />
+      <div classname="row">
+        <h2>Upcoming</h2>
+        <div className="row__posters">
+          {upcoming?.map((e) => (
+            <div>
+              {/* <h3 key={e.id}>{e.name}</h3> */}
+              <img src={e.img} className="row__poster" />
+            </div>
+          )
+          )}
+        </div>
+      </div>
     </div>
   );
 }
