7652920301890c2f427ee9555b334bb7deacd7ff
a73heredia <cristianalejandroheredia@gmail.com>
2022-03-03 23:35:15 -0300

    8com

--- PF-Movie-Mark/api/src/models/Movie.js
+++ PF-Movie-Mark/api/src/models/Movie.js
@@ -4,13 +4,8 @@ module.exports = (sequelize) => {
   sequelize.define("movie", {
     id: {
       type: DataTypes.INTEGER,
-<<<<<<< HEAD
-      primaryKey: true,
-      
-=======
       allowNull: false,
       primaryKey: true, 
->>>>>>> 93ed76cdd9305e109f0ccf81bc34299ea5cf23c9
     },
     title: {
       type: DataTypes.STRING,
