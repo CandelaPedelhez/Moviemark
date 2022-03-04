0c0cb7e4174e3646b098558201115f532ff9986e
Juan <jnmvrgr@gmail.com>
2022-03-04 05:59:00 -0300

    hardcoded data

---
+++ PF-Movie-Mark/client/src/Components/constants/upcoming.js
@@ -0,0 +1,31 @@
+export const upcoming = [
+    {
+        id: 1,
+        name: "Umma",
+        img: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/eHR30gzdRpAkDGcCAGmJ9pW6QXi.jpg"
+    },
+    {
+        id: 2,
+        name: "El lobo y el leon",
+        img: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/6gVO98lUJuzEbdlM5zV5iPwQr8a.jpg"
+    },
+    {
+        id: 3,
+        name: "La peor persona del mundo",
+        img: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/1eOElXpU3y7dkH2p9jn3WPArA6V.jpg"
+    },
+    {
+        id: 4,
+        name: "The Duke",
+        img: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/fFeViNHhxXttSYZJH68mQuw3gj3.jpg"
+    },
+    {
+        id: 5,
+        name: "Gold",
+        img: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/nDF3Ryi3W8PIR1b5eXDWBMRzgTS.jpg"
+    },
+]
+
+export default {
+    upcoming,
+}
