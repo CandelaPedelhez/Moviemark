4999f70b84b47ed1137f81c87d90f2dd1da9d605
Nacarith <nacariths@gmail.com>
2022-03-05 22:16:28 -0400

    Models groceries fixed

--- PF-Movie-Mark/api/src/models/Groceries.js
+++ PF-Movie-Mark/api/src/models/Groceries.js
@@ -2,17 +2,28 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("groceries", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+    },
     name: {
       type: DataTypes.STRING,
     },
     price: {
       type: DataTypes.FLOAT,
     },
-    stock: {
-      type: DataTypes.INTEGER,
+    typeGrocerie:{
+      type: DataTypes.STRING
     },
-    img: {
+    description: {
       type: DataTypes.STRING,
+    },
+    /*stock: {
+      type: DataTypes.INTEGER,
+    },*/
+    img: {
+      type: DataTypes.TEXT,
     },
   });
 };
