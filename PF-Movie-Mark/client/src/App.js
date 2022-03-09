a85d85f4327340e560ac871a7f6aa35a4966c4e1
Juan <jnmvrgr@gmail.com>
2022-03-09 05:53:02 -0300

    added cart provider and payment route

--- PF-Movie-Mark/client/src/App.js
+++ PF-Movie-Mark/client/src/App.js
@@ -3,11 +3,13 @@ import LandingPage from "./Components/Landing/Landing";
 import Home from "./Components/Home";
 import Groceries from "./Components/Groceries";
 import Details from "./Components/Details";
-import ShoppingCart from "./Components/ShoppingCart";
+import Payment from "./Components/Payment";
+import { CartProvider } from './Context/CartContext';
 import "./App.css"
 
 function App() {
   return (
+    <CartProvider>
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
@@ -16,9 +18,10 @@ function App() {
         <Route path='/movies/:id' element={<Details movies="movies"/>} />
         <Route path='/upcoming/:id' element={<Details movies="upcoming"/>} />
         <Route path='/toprated/:id' element={<Details movies="toprated"/>} />
-        <Route path='/cart' element={<ShoppingCart/>} />
+        <Route path='/payment' element={<Payment/>} />
       </Routes>
     </BrowserRouter>
+    </CartProvider>
   );
 }
 
