60cbbf7f23d907507eee332a7be426345e80bef9
Juan <jnmvrgr@gmail.com>
2022-03-09 05:52:31 -0300

    deleted commented line of code

--- PF-Movie-Mark/client/src/Actions/index.js
+++ PF-Movie-Mark/client/src/Actions/index.js
@@ -109,12 +109,4 @@ export function orderBy(payload) {
   return { type: "ORDER_BY", payload };
 }
 
-export const TYPES = {
-  ADD_TO_CART: "ADD_TO_CART",
-  REMOVE_ONE_FROM_CART: "REMOVE_ONE_FROM_CART",
-  REMOVE_ALL_FROM_CART: "REMOVE_ALL_FROM_CART",
-  CLEAR_CART: "CLEAR_CART",
-  TOTAL_AMOUNT: "TOTAL_AMOUNT"
-}
-
 
