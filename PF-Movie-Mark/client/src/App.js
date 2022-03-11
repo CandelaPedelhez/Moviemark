58db6b57763d0a6832764442398fa75470209564
Juan <jnmvrgr@gmail.com>
2022-03-11 06:33:10 -0300

    min changes

--- PF-Movie-Mark/client/src/App.js
+++ PF-Movie-Mark/client/src/App.js
@@ -3,7 +3,7 @@ import LandingPage from "./Components/Landing/Landing";
 import Home from "./Components/Home/Home.jsx";
 import Groceries from "./Components/Groceries/Groceries.jsx";
 import Details from "./Components/Details/Details.jsx";
-import Payment from "./Components/Payment";
+import Receipt from "./Components/Receipt/Receipt.jsx";
 import { CartProvider } from './Context/CartContext';
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/SignUp/SignUp";
@@ -22,7 +22,7 @@ function App() {
         <Route path='/movies/:id' element={<Details movies="movies"/>} />
         <Route path='/upcoming/:id' element={<Details movies="upcoming"/>} />
         <Route path='/toprated/:id' element={<Details movies="toprated"/>} />
-        <Route path='/payment' element={<Payment/>} />
+        <Route path='/payment' element={<Receipt/>} />
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<SignUp />} />
         <Route path='/resetpassword' element={<ResetPassword />} />
