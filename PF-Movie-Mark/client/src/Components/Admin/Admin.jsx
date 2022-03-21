847ce0a5be02b11209ef31c6b0a4d0f6d4df60a5
francedermaz <francedermaz@gmail.com>
2022-03-21 11:10:22 -0300

    Fix error 404 - reload

--- PF-Movie-Mark/client/src/Components/Admin/Admin.jsx
+++ PF-Movie-Mark/client/src/Components/Admin/Admin.jsx
@@ -160,7 +160,7 @@ const Admin = () => {
         setCharging(true);
         dispatch(logoutUser())
         .then(()=>{
-            setTimeout( function() { history('/home'); }, 2000 )
+            setTimeout( function() { window.location.href ='http://localhost:3000/home'; }, 2000 )
         })
     }
 
